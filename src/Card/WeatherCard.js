6b8aa1254aff912783c96def5e56b7e1f8cfefed
bipul8282 <119596693+bipul8282@users.noreply.github.com>
2023-09-28 20:48:13 +0530

    complete

--- src/Card/WeatherCard.js
+++ src/Card/WeatherCard.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import WbSunnyIcon from '@mui/icons-material/WbSunny';
-import './WeatherCard.css';
+import "./WeatherCard.css";
 
 function WeatherCard(props) {
-  const { city, temp } = props;
-
   return (
-    <div className="cardContainer">
-      <div className="cardContainer1">
-        <h2>{city}</h2>
+    <div className='cardContainer'>
+      <div className='cardContainer1'>
+        <h2>{props.city}</h2>
         <p>India</p>
-        <div className="detailsIcon">
-          <h1>
+        <div className='detailsIcon'>
+          <h1 className='temperature'>
             <WbSunnyIcon style={{ fontSize: "50px", marginRight: "1rem" }} />
-            {temp}&deg;C
+            <span className='rainbowText'>
+              {props.temp}&deg;{props.unit === 'metric' ? 'C' : 'F'}
+            </span>
           </h1>
         </div>
+        <select id="unitSelector" onChange={props.onUnitChange} value={props.unit}>
+          <option value="metric">Celsius</option>
+          <option value="imperial">Fahrenheit</option>
+        </select>
       </div>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -8,42 +8,46 @@ function Home() {
   const [city, setCity] = useState("");
   const [temp, setTemp] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [unit, setUnit] = useState("metric"); 
 
   const handleChange = (e) => {
     setValue(e.target.value);
   };
 
+  const handleUnitChange = (e) => {
+    setUnit(e.target.value);
+    fetchApi();
+  };
+
   const fetchApi = async () => {
-    setIsLoading(true); 
+    setIsLoading(true);
     try {
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=b24982bca818f3dea1c76357d4773f19`)
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${value}&units=${unit}&appid=b24982bca818f3dea1c76357d4773f19`);
       if (response.ok) {
         const data = await response.json();
-        console.log(data); 
+        console.log(data);
         setCity(data.name);
-        setTemp((data.main.temp - 273.15).toFixed(1));
+        setTemp(data.main.temp);
       } else {
         console.error('Error fetching weather data:', response.statusText);
       }
     } catch (error) {
       console.error('Error fetching weather data:', error);
     } finally {
-      setIsLoading(false); 
+      setIsLoading(false);
     }
   };
 
   return (
     <div className="weatherWrapper">
       <div className="header">
-        <h1>
-          Weather Report
-        </h1>
+        <h1>Weather Web App</h1>
       </div>
       <div className="SearchFilter">
         <Search
           onChange={handleChange}
           value={value}
-          onSearch={fetchApi} 
+          onSearch={fetchApi}
           isLoading={isLoading}
         />
       </div>
@@ -51,13 +55,14 @@ function Home() {
         {isLoading ? (
           <div className="loadingIcon">Loading...</div>
         ) : (
-          <WeatherCard city={city} temp={temp} />
+          <WeatherCard
+            city={city}
+            temp={temp}
+            unit={unit}
+            onUnitChange={handleUnitChange}
+          />
         )}
       </div>
-      
-=  
-
-
     </div>
   );
 }
